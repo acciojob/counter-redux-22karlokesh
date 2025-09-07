2a85c45fe077f93441ceb754c968d28ee702189b
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-09-08 00:18:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,49 +6,17 @@
 
 
 // my code
+
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider, useDispatch, useSelector } from "react-redux";
-
-const initialState = { count: 0 };
-
-
-const increment = () => ({ type: "INCREMENT" });
-const decrement = () => ({ type: "DECREMENT" });
-
-function counterReducer(state = initialState, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return { count: state.count + 1 };
-    case "DECREMENT":
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(counterReducer);
-
-const CounterApp = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment())}>increment</button>
-      <button onClick={() => dispatch(decrement())}>decrement</button>
-    </div>
-  );
-};
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <CounterApp />
+    <App />
   </Provider>,
   document.getElementById("root")
 );
 
-// above my code
-ReactDOM.render(<App />, document.getElementById("root"));
